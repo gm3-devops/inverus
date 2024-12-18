d3b583f285966a4828511ed4eaa73d0502526cf7
bobirrahmatov <55207867+bobirrahmatov@users.noreply.github.com>
2024-12-18 16:16:31 +0100

    added footer margins

--- src/components/marketing/footer.tsx
+++ src/components/marketing/footer.tsx
@@ -6,8 +6,8 @@ import { ShieldCheckIcon } from 'lucide-react';
 const Footer = () => {
     return (
         <footer className="flex flex-col relative items-center justify-center border-t border-border pt-16 pb-8 md:pb-0 px-6 lg:px-8 w-full max-w-6xl mx-auto lg:pt-32 bg-[radial-gradient(35%_128px_at_50%_0%,theme(backgroundColor.white/8%),transparent)]">
-            <div className="absolute top-0 left-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-1.5 bg-primary rounded-full">
-            </div>
+            <div className="absolute top-0 left-1/2 right-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-1.5 bg-primary rounded-full"></div>
+
             <div className="grid gap-8 xl:grid-cols-3 xl:gap-8 w-full">
                 <Container delay={0.1}>
                     <div className="flex flex-col items-start justify-start md:max-w-[200px]">
@@ -31,14 +31,11 @@ const Footer = () => {
                         </div>
                     </div>
                 </Container>
-
-                <div className="grid-cols-2 gap-8 grid mt-16 xl:col-span-2 xl:mt-0">
+                <div className="grid grid-cols-2 gap-8 mt-4 xl:col-span-2 xl:mt-0">
                     <div className="md:grid md:grid-cols-2 md:gap-8">
                         <Container delay={0.2}>
-                            <div className="">
-                                <h3 className="text-base font-medium text-white">
-                                    Industries
-                                </h3>
+                            <div>
+                                <h3 className="text-base font-medium text-white">Industries</h3>
                                 <ul className="mt-4 text-sm text-muted-foreground">
                                     <li className="mt-2">
                                         <Link href="" className="hover:text-foreground transition-all duration-300">
@@ -64,12 +61,11 @@ const Footer = () => {
                             </div>
                         </Container>
                     </div>
+
                     <div className="md:grid md:grid-cols-2 md:gap-8">
                         <Container delay={0.4}>
-                            <div className="">
-                                <h3 className="text-base font-medium text-white">
-                                    Company
-                                </h3>
+                            <div>
+                                <h3 className="text-base font-medium text-white">Company</h3>
                                 <ul className="mt-4 text-sm text-muted-foreground">
                                     <li className="mt-2">
                                         <Link href="/resources/blog" className="hover:text-foreground transition-all duration-300">
@@ -92,19 +88,21 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="mt-8 border-t border-border/40 pt-4 md:pt-8 md:flex md:items-center md:justify-between w-full flex-wrap">
+
+            <div className="mt-8 border-t border-border/40 pt-4 md:pt-8 flex flex-col items-center md:flex-row md:justify-between w-full">
                 <Container delay={0.5}>
-                    <p className="text-sm text-muted-foreground mt-8 md:mt-0 md:ml-auto">
+                    <p className="text-sm text-muted-foreground text-center md:text-left">
                         &copy; {new Date().getFullYear()} inVerus Inc. All rights reserved.
-                        <span>{" "}|{" "} </span>
-                        <Link href="/privacy-policy" className="hover:text-foreground transition-all duration-300">
+                    </p>
+                    {/* <div className="flex gap-4 mt-4 md:mt-0">
+                        <Link href="/privacy-policy" className="text-sm text-muted-foreground hover:text-foreground transition-all duration-300">
                             Privacy Policy
                         </Link>
-                        <span>{" "}|{" "}</span>
-                        <Link href="/terms-and-conditions" className="hover:text-foreground transition-all duration-300">
+                        <span>|</span>
+                        <Link href="/terms-and-conditions" className="text-sm text-muted-foreground hover:text-foreground transition-all duration-300">
                             Terms & Conditions
                         </Link>
-                    </p>
+                    </div> */}
                 </Container>
             </div>
         </footer>
