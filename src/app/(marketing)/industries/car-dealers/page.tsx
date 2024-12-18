16f9e1a3bb2a00a4b463bbb29d83381854c1f033
bobirrahmatov <55207867+bobirrahmatov@users.noreply.github.com>
2024-12-18 22:43:02 +0100

    solutions added

--- src/app/(marketing)/industries/car-dealers/page.tsx
+++ src/app/(marketing)/industries/car-dealers/page.tsx
@@ -1,9 +1,7 @@
 import Container from "@/components/global/container";
 import Wrapper from "@/components/global/wrapper";
-import { Button } from "@/components/ui/button";
 import MagicBadge from "@/components/ui/magic-badge";
 import Image from "next/image";
-import Link from "next/link";
 import { ScheduleCall } from "@/components/ui/cal";
 import { fraud } from "@/constants/industries";
 import { Icons } from "@/components";
@@ -38,12 +36,12 @@ const LinkShorteningPage = () => {
                     </div>
                 </Container>
                 <Container className="pt-10">
-                    <div className="max-w-md mx-auto text-start md:text-center">
+                    <div className="max-w-2xl mx-auto mx-auto text-start md:text-center">
                         <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
                             The Fraud Challenge Across Automotive Ecosystem
                         </h2>
                         <p className="text-muted-foreground mt-6">
-                            Astra offers a range of features to help you build a stunning website in no time
+                            Uncovering vulnerabilities across the automotive landscape, from dealerships to DMS providers and car brands, and the strategies needed to combat them.
                         </p>
                     </div>
                 </Container>
@@ -71,6 +69,109 @@ const LinkShorteningPage = () => {
                         </div>
                     </div>
                 </Container>
+            </Wrapper>
+            <Wrapper className="flex flex-col items-center justify-center py-8 md:py-12 w-full">
+                <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
+                    <div className="max-w-2xl mx-auto mx-auto text-start md:text-center">
+                        <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+                            How inVerus Delivers for Auto Industry Leaders
+                        </h2>
+                    </div>
+                    <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
+                        <div className="relative lg:row-span-2">
+                            <div className="absolute inset-px rounded-lg bg-background [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] border border-border lg:rounded-l-[2rem]"></div>
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+                                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                                    <h3 className="font-semibold text-neutral-300 text-xl">
+                                        Consortium Intelligence
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        Shared real-time data on fraudulent behaviors across the dealer network.
+                                    </p>
+                                </div>
+                                <div className="relative min-h-[30rem] w-full grow [container-type:inline-size] max-lg:mx-auto max-lg:max-w-sm">
+                                    <div className="absolute inset-x-10 bottom-0 top-10 overflow-hidden rounded-t-[12cqw] border-x-[3cqw] border-t-[3cqw] border-gray-700 bg-gray-900 shadow-2xl">
+                                        <img
+                                            className="size-full object-cover object-top"
+                                            src="https://tailwindui.com/plus/img/component-images/bento-03-mobile-friendly.png"
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+                        </div>
+                        <div className="relative max-lg:row-start-1">
+                            <div className="absolute inset-px rounded-lg bg-background [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] border border-border  max-lg:rounded-t-[2rem]"></div>
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+                                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                                    <h3 className="font-semibold text-neutral-300 text-xl">
+                                        Harvestor / Reverse Search Engine
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        Pinpoint fraudsters and repeat offenders through advanced search.
+                                    </p>
+                                </div>
+                                <div className="flex flex-1 items-center justify-center px-8 max-lg:pb-12 max-lg:pt-10 sm:px-10 lg:pb-2">
+                                    <img
+                                        className="w-full max-lg:max-w-xs"
+                                        src="https://tailwindui.com/plus/img/component-images/bento-03-performance.png"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+                        </div>
+                        <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
+                            <div className="absolute inset-px rounded-lg bg-background [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] border border-border "></div>
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+                                <div className="px-8 pt-8 sm:px-10 sm:pt-10">
+                                    <h3 className="font-semibold text-neutral-300 text-xl">
+                                        Behavioral Insights
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        Detect anomalies in buyer behavior during test drives, warranty claims, and purchase cycles.
+                                    </p>
+                                </div>
+                                <div className="flex flex-1 items-center [container-type:inline-size] max-lg:py-6 lg:pb-2">
+                                    <img
+                                        className="h-[min(152px,40cqw)] object-cover"
+                                        src="https://tailwindui.com/plus/img/component-images/bento-03-security.png"
+                                        alt=""
+                                    />
+                                </div>
+                            </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
+                        </div>
+                        <div className="relative lg:row-span-2">
+                            <div className="absolute inset-px rounded-lg bg-background [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] border border-border  max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+                            <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                                    <h3 className="font-semibold text-neutral-300 text-xl">
+                                        Solution 4
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget sem sodales gravida.
+                                    </p>
+                                </div>
+                                <div className="relative min-h-[30rem] w-full grow">
+                                    <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                                        <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                                            <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                                                <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                                                    NotificationSetting.jsx
+                                                </div>
+                                                <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
+                                            </div>
+                                        </div>
+                                        <div className="px-6 pb-14 pt-6">{/* Your code example */}</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+                        </div>
+                    </div>
+                </div>
             </Wrapper>
         </>
     )
