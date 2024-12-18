958013c9de64f39b6212614e9a1feb551e06f08a
bobirrahmatov <55207867+bobirrahmatov@users.noreply.github.com>
2024-12-18 21:50:31 +0100

    car dealers

--- src/app/(marketing)/industries/car-dealers/page.tsx
+++ src/app/(marketing)/industries/car-dealers/page.tsx
@@ -26,46 +26,49 @@ const LinkShorteningPage = () => {
                         </div>
                     </div>
                 </Container>
-                {/* <Container>
+                <Container delay={0.2} className="w-full">
+                    <div className="w-full flex max-w-4xl py-2 mx-auto">
+                        <Image
+                            src="/images/industries/qr-codes.svg"
+                            alt="Create QR codes for your links"
+                            width={80}
+                            height={80}
+                            className="w-full h-auto"
+                        />
+                    </div>
+                </Container>
+                <Container className="pt-10">
+                    <div className="max-w-md mx-auto text-start md:text-center">
+                        <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+                            The Fraud Challenge Across Automotive Ecosystem
+                        </h2>
+                        <p className="text-muted-foreground mt-6">
+                            Astra offers a range of features to help you build a stunning website in no time
+                        </p>
+                    </div>
+                </Container>
+                <Container>
                     <div className="flex items-center justify-center mx-auto mt-0">
                         <Icons.feature className="w-auto h-80" />
                     </div>
-                </Container> */}
+                </Container>
                 <Container>
-                    <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                        <div className="lg:pr-8">
-                            <div className="lg:max-w-lg">
-                                <h2 className="text-3xl lg:text-4xl font-semibold font-heading mt-6 !leading-tight">
-                                    The Fraud Challenge Across Automotive Ecosystem
-                                </h2>
-                                <p className="text-base md:text-lg mt-6 text-muted-foreground">
-                                    Uncovering vulnerabilities across the automotive landscape, from dealerships to DMS providers and car brands, and the strategies needed to combat them.
-                                </p>
-                                <dl className="mt-10 max-w-xl space-y-8  lg:max-w-none">
-                                    {fraud.map((feature) => (
-                                        <div key={feature.title} className="relative pl-9">
-                                            <dt className="inline font-semibold">
-                                                <feature.icon
-                                                    aria-hidden="true"
-                                                    className="absolute left-1 top-1 size-5"
-                                                />
-                                                {feature.title}
-                                            </dt>{' '}
-                                            <dd className="inline text-muted-foreground">
-                                                {feature.info}
-                                            </dd>
-                                        </div>
-                                    ))}
-                                </dl>
-                            </div>
+                    <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
+                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-8">
+                            {fraud.map((feature) => (
+                                <div key={feature.title} className="flex flex-col items-start lg:items-start px-0 md:px-0">
+                                    <div className="flex items-center justify-center">
+                                        <feature.icon className="w-8 h-8" />
+                                    </div>
+                                    <h3 className="text-lg font-medium mt-4">
+                                        {feature.title}
+                                    </h3>
+                                    <p className="text-muted-foreground mt-2 text-start lg:text-start">
+                                        {feature.info}
+                                    </p>
+                                </div>
+                            ))}
                         </div>
-                        <img
-                            alt="Car Dealers"
-                            src="/images/industries/car-dealers.svg"
-                            width={2432}
-                            height={1442}
-                            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-                        />
                     </div>
                 </Container>
             </Wrapper>
